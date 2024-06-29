2ccb4f7146b77c1ff2a667458da5b6f106c1479d
Jawad Ali <jawadali000123@gmail.com>
2024-06-30 01:24:50 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-800 h-screen'>
+        <h1>React App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
