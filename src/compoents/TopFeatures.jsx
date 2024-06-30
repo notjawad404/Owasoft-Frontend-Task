f45a9cc1450fd5c518eece21ab624eb7b36a2ba3
Jawad Ali <jawadali000123@gmail.com>
2024-06-30 18:14:48 +0500

    update TopFeatures section

--- src/compoents/TopFeatures.jsx
+++ src/compoents/TopFeatures.jsx
@@ -1,5 +1,15 @@
 import PropTypes from 'prop-types';
 
+import featuretop from '../assets/Features/featurebg.svg'
+import arrowIcon from "../assets/Features/arrowIcon.svg"
+
+import servicesIcon1 from '../assets/Features/servicesIcon1.svg';
+import servicesIcon2 from '../assets/Features/servicesIcon2.svg';
+import servicesIcon3 from '../assets/Features/servicesIcon3.svg';
+import servicesIcon4 from '../assets/Features/servicesIcon4.svg';
+import servicesIcon5 from '../assets/Features/servicesIcon5.svg';
+import servicesIcon6 from '../assets/Features/servicesIcon6.svg';
+
 
 const FeatureCard = ({ title, description, icon, middle }) => {
 
@@ -8,14 +18,15 @@ const FeatureCard = ({ title, description, icon, middle }) => {
     : 'shadow-md';
 
   return (
-    <div className={`w-full md:w-1/3 p-4 ${middle ? 'md:mt-8' : ''}`}>
+    <div className={`w-1/3 p-4 ${middle ? 'mt-8' : ''}`}>
       <div className={`bg-white rounded-lg overflow-hidden ${cardClasses}`}>
-        <div className="p-4 flex items-center justify-center">
-          <span className="text-4xl">{icon}</span>
-        </div>
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{title}</div>
-          <p className="text-gray-700 text-base">{description}</p>
+        <div className="pt-8 pb-10 px-5">
+        <img src={icon} alt="ServicesIcon" className="py-5"/>
+          <h1 className="text-2xl font-medium py-5">{title}</h1>
+          <p className=" font-normal text-lg py-5">{description}</p>
+        <div className=" font-medium text-xl hover:px-7 py-5 flex flex-row">
+        <p>Read More </p>
+        <img src={arrowIcon} alt="ArrowIcon" className=" px-2 pt-1"/></div>
         </div>
       </div>
     </div>
@@ -27,37 +38,37 @@ export default function TopFeatures() {
     {
       title: "Scheduled Publishing",
       description: "Allow users to schedule the publication of alert messages at specific dates and times, enhancing timely communication.",
-      icon: "🕑",
+      icon: servicesIcon1,
       middle: false
     },
     {
       title: "Customizable Embed Codes",
       description: "Empower users with the ability to customize embed codes, tailoring the appearance of alert messages on their websites.",
-      icon: "💻",
+      icon: servicesIcon2,
       middle: true
     },
     {
       title: "Analytics Dashboard",
       description: "Provide a comprehensive analytics dashboard, offering insights into user engagement and performance metrics for alert messages.",
-      icon: "📊",
+      icon: servicesIcon3,
       middle: false
     },
     {
       title: "Multi-Website Support",
       description: "Enable users to manage and create customized alert messages for multiple websites within a single account and publish on multiple websites.",
-      icon: "🌐",
+      icon: servicesIcon4,
       middle: false
     },
     {
       title: "Responsive Interface",
       description: "Design a responsive and user-friendly interface, ensuring a seamless experience across various devices and screen sizes.",
-      icon: "📱",
+      icon: servicesIcon5,
       middle: true
     },
     {
       title: "Rolebased User Management",
       description: "Implement role-based user management, allowing different authorization levels such as admin and user for enhanced security.",
-      icon: "🛡️",
+      icon: servicesIcon6,
       middle: false
     }
   ];
@@ -71,10 +82,13 @@ export default function TopFeatures() {
 
 
   return (
-    <div className="flex flex-wrap justify-center">
-      {featureCards.map((card, index) => (
-        <FeatureCard key={index} {...card} />
-      ))}
+    <div className="pt-20">
+    <img src={featuretop} alt='featuretop' className="w-full"/>
+    <div className="flex flex-wrap justify-center bg-blue-50 px-20">
+    {featureCards.map((card, index) => (
+      <FeatureCard key={index} {...card} />
+    ))}
+  </div>
     </div>
   );
 }
