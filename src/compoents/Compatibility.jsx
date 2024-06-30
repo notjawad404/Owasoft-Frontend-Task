330e54e3241e337431ce4f967f063d42255ec06d
Jawad Ali <jawadali000123@gmail.com>
2024-06-30 19:54:33 +0500

    Add Compatibility section

--- src/compoents/Compatibility.jsx
+++ src/compoents/Compatibility.jsx
@@ -1,34 +1,38 @@
+import '../App.css'
 
+import chrome from '../assets/chrome.svg'
+import firefox from '../assets/firefox.svg'
+import safari from '../assets/safari.svg'
 
 const CompatibilityList = [
   {
-    logo: "S",
+    logo: chrome,
     name: "Chrome",
     details: "Bolster website retention rate with Windows & Android OS compatible push notifications. Easily send web push notifications on any device.",
   },
   {
-    logo: "F",
+    logo: firefox,
     name: "Firefox",
-    details: "Seamlessly send push notifications on Firefox across Windows, macOS, and Linux operating systems.",
+    details: "Maximize your ROI with Firefox browser compatible notifications. Send real-time push notifications on smart phones & desktops.",
   },
   {
-    logo: "E",
-    name: "Edge",
-    details: "Deliver push notifications on Microsoft Edge to engage users on both Windows and macOS platforms.",
+    logo: safari,
+    name: "Safari",
+    details: "Skyrocket site conversion rate with Safari browser compatible. We use Apple Notification Services to effectively deliver your message.",
   },
 ];
 
 const Compatibility = () => {
   return (
-    <div className="flex justify-center items-center py-10">
-    <h1 className="">Compatibility</h1>
-    <h1 className=" font-bold text-4xl">Support for Popular Web Browsers</h1>
+    <div className="py-10 fontRubik">
+    <h1 className="text-center text-lg font-normal">Compatibility</h1>
+    <h1 className="text-center font-bold text-4xl">Support for Popular Web Browsers</h1>
       <div className="flex flex-wrap justify-center px-10">
         {CompatibilityList.map((list, index) => (
-          <div className="w-1/3" key={index}>
-          <div className="bg-white shadow-md rounded-lg p-4">
-          <h1 className="text-3xl font-bold mb-2">{list.logo}</h1>
-          <h1 className="font-semibold text-xl mb-2">{list.name}</h1>
+          <div className="w-1/3 text-center " key={index}>
+          <div className="bg-white h-[419px] flex flex-col justify-center items-center  shadow-md rounded-lg py-12 px-16 ">
+          <img src={list.logo} alt='logo' className="py-10"/>
+          <h1 className="font-medium text-2xl my-2">{list.name}</h1>
           <p className="text-gray-700">{list.details}</p>
           </div>
         </div>
